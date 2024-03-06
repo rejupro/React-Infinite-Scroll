0a8c424ed7057b3b12815281e18c0fb9b76a9f7b
Md. Reedwanul Haque <reedwanultpi@gmail.com>
2024-03-06 22:27:50 +0600

    Complete Infinite Load

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import "./App.css";
+import ProductList from "./components/ProductList";
 
 function App() {
   return (
     <>
-      <h2 className="font-bold">Hello Reju</h2>
+      <ProductList />
     </>
   );
 }
---
+++ src/components/ProductList.jsx
@@ -0,0 +1,67 @@
+import { useEffect, useRef, useState } from "react";
+import Product from "./Product";
+
+const productsPerPage = 10;
+
+export default function ProductList() {
+  const [products, setProducts] = useState([]);
+  const [page, setPage] = useState(0);
+  const [hasMore, setHasMore] = useState(true);
+  const loaderRef = useRef(null);
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const response = await fetch(
+        `https://dummyjson.com/products?limit=${productsPerPage}&skip=${
+          page * productsPerPage
+        }`
+      );
+      const data = await response.json();
+
+      if (data.products.length === 0) {
+        setHasMore(false);
+      } else {
+        setProducts((prevProducts) => [...prevProducts, ...data.products]);
+        setPage((prevPage) => prevPage + 1);
+      }
+    };
+
+    const onIntersection = (items) => {
+      const loaderItem = items[0];
+
+      if (loaderItem.isIntersecting && hasMore) {
+        fetchProducts();
+      }
+    };
+
+    const observer = new IntersectionObserver(onIntersection);
+
+    if (observer && loaderRef.current) {
+      observer.observe(loaderRef.current);
+    }
+
+    // cleanup
+    return () => {
+      if (observer) observer.disconnect();
+    };
+  }, [hasMore, page]);
+
+  return (
+    <>
+      <h2 className="font-bold text-center text-2xl my-0.5">Product List</h2>
+      {products.map((product) => (
+        <Product
+          title={product.title}
+          price={product.price}
+          thumbnail={product.thumbnail}
+          key={product.id}
+        />
+      ))}
+      {hasMore ? (
+        <div ref={loaderRef}>Loading more products...</div>
+      ) : (
+        <div>No More Products</div>
+      )}
+    </>
+  );
+}
